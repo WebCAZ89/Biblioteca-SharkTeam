defd9b4386f8b8b6e3ac755e5d3e7470e4c1a07c
laurazorro <56643359+laurazorro@users.noreply.github.com>
2021-09-29 20:08:37 -0500

    Proyecto Biblioteca

---
+++ MisionTIC/MisionTIC/Models/viewModels/Autors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MisionTIC.Models.viewModels
+{
+    public class Autors
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Nacionalidad { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
