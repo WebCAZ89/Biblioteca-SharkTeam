a41d1a8bd9e671f1dc912260cf866ec54f9d8298
WebCAZ89 <webcaz89@gmail.com>
2021-09-30 22:39:39 -0500

    Creación de Modelo y Controlador entidad Libro.

---
+++ MisionTIC/MisionTIC/Models/viewModels/Libro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MisionTIC.Models.viewModels
+{
+    public class Libro
+    {
+        
+        public string Nombre { get; set; }
+        public int Tipo { get; set; }
+        public string Editorial { get; set; }
+        public int Ano { get; set; }
+        public string Autor { get; set; }
+    }
+}
+    
