80e10fc3f5a5e43f6163ffcc3244f50f1e596a41
laurazorro <56643359+laurazorro@users.noreply.github.com>
2021-10-10 14:24:28 -0500

    Crud copia y lector sin estilos

---
+++ MisionTIC/MisionTIC/Controllers/CopiasController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MisionTIC.Models;
+
+namespace MisionTIC.Controllers
+{
+    public class CopiasController : Controller
+    {
+        private BibliotecaTicEntities db = new BibliotecaTicEntities();
+
+        // GET: Copias
+        public ActionResult Index()
+        {
+            var copia = db.Copia.Include(c => c.Libro);
+            return View(copia.ToList());
+        }
+
+        // GET: Copias/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Copia copia = db.Copia.Find(id);
+            if (copia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(copia);
+        }
+
+        // GET: Copias/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdLibro = new SelectList(db.Libro, "IdLibro", "NombreLibro");
+            return View();
+        }
+
+        // POST: Copias/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdCopia,Estado,IdLibro")] Copia copia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Copia.Add(copia);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdLibro = new SelectList(db.Libro, "IdLibro", "TipoLibro", copia.IdLibro);
+            return View(copia);
+        }
+
+        // GET: Copias/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Copia copia = db.Copia.Find(id);
+            if (copia == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdLibro = new SelectList(db.Libro, "IdLibro", "TipoLibro", copia.IdLibro);
+            return View(copia);
+        }
+
+        // POST: Copias/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdCopia,Estado,IdLibro")] Copia copia)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(copia).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdLibro = new SelectList(db.Libro, "IdLibro", "TipoLibro", copia.IdLibro);
+            return View(copia);
+        }
+
+        // GET: Copias/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Copia copia = db.Copia.Find(id);
+            if (copia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(copia);
+        }
+
+        // POST: Copias/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Copia copia = db.Copia.Find(id);
+            db.Copia.Remove(copia);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MisionTIC/MisionTIC/Controllers/LectorsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MisionTIC.Models;
+
+namespace MisionTIC.Controllers
+{
+    public class LectorsController : Controller
+    {
+        private BibliotecaTicEntities db = new BibliotecaTicEntities();
+
+        // GET: Lectors
+        public ActionResult Index()
+        {
+            return View(db.Lector.ToList());
+        }
+
+        // GET: Lectors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lector lector = db.Lector.Find(id);
+            if (lector == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lector);
+        }
+
+        // GET: Lectors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Lectors/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Identifiacion,NombreLector,FechaNacimientoLec,TelefonoLector,CorreoLector")] Lector lector)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Lector.Add(lector);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(lector);
+        }
+
+        // GET: Lectors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lector lector = db.Lector.Find(id);
+            if (lector == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lector);
+        }
+
+        // POST: Lectors/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Identifiacion,NombreLector,FechaNacimientoLec,TelefonoLector,CorreoLector")] Lector lector)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(lector).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(lector);
+        }
+
+        // GET: Lectors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lector lector = db.Lector.Find(id);
+            if (lector == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lector);
+        }
+
+        // POST: Lectors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Lector lector = db.Lector.Find(id);
+            db.Lector.Remove(lector);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
